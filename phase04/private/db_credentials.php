439dcd9b5d9b5a20935f1fa72412c74728ed5b34
Luciano Menezes <lucianomenezes13@gmail.com>
2022-04-27 20:31:53 -0700

    Phase04 final commit

--- phase04/private/db_credentials.php
+++ phase04/private/db_credentials.php
@@ -1,12 +1,12 @@
 <?php
 // local
-define("DB_SERVER", "localhost");
-define("DB_USER", "sally");
-define("DB_PASS", "somePa55word");
-define("DB_NAME", "salamanders");
+define("DB_SERVER", "");
+define("DB_USER", "");
+define("DB_PASS", "");
+define("DB_NAME", "");
 
-//a2
-// define("DB_SERVER", "localhost");
-// define("DB_USER", "charli12_doall");
-// define("DB_PASS", "ErYje7HV2o2Lqp7B9gx");
-// define("DB_NAME", "charli12_salamanders");
+//WebHost
+// define("DB_SERVER", "");
+// define("DB_USER", "");
+// define("DB_PASS", "");
+// define("DB_NAME", "");
