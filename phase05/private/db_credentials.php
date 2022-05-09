8e80d5794e7d127f2693142a49b34a1436dd69a4
Luciano Menezes <lucianomenezes13@gmail.com>
2022-05-09 14:48:11 -0700

    Phase05 complete update

--- phase05/private/db_credentials.php
+++ phase05/private/db_credentials.php
@@ -1,14 +1,14 @@
 <?php
 // local
 
-define("DB_SERVER", "localhost");
-define("DB_USER", "sally");
-define("DB_PASS", "somePa55word");
-define("DB_NAME", "salamanders");
+// define("DB_SERVER", "");
+// define("DB_USER", "");
+// define("DB_PASS", "");
+// define("DB_NAME", "");
 
 // //WebHost
 
-// define("DB_SERVER", "localhost");
-// define("DB_USER", "ug22xiu8k9gcd");
-// define("DB_PASS", "bnzxboknbpoy");
-// define("DB_NAME", "dbajpq1tj89ouk");
+// define("DB_SERVER", "");
+// define("DB_USER", "");
+// define("DB_PASS", "");
+// define("DB_NAME", "");
